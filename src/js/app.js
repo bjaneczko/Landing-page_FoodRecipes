28510a1fd75b4060b0f94011bd34f1d43287c621
Bartlomiej Janeczko <janeczko.bartlomiej@gmail.com>
2022-05-14 23:40:53 +0200

    Changing import of scroll reveal

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,6 @@
-import "../scss/app.scss";
 import "remixicon/fonts/remixicon.css";
+
+import "../scss/app.scss";
+
 import "./hamburger-menu.js";
-import "./scrollreveal.min.js";
 import "./scroll.js";
